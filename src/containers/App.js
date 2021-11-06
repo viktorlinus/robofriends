6cfb1218b6f92e3641bdc6b68227d1ed8039c921
viktorlinus <viktorlinus@hotmail.com>
2021-11-06 08:48:46 +0100

    adding redux to rest of app, now with ajax and api call

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,43 +3,38 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
-import {setSearchField} from '../actions';
+import {setSearchField, requestRobots} from '../actions';
 import {connect} from 'react-redux';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: []
-    }
-  }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+    this.props.onRequestRobots();
   }
 
 
   render() {
-    const { robots } = this.state;
-    const {onSearchChange,searchField} = this.props;
+    const {onSearchChange,searchField, robots, isPending} = this.props;
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    return !robots.length ?
+    return isPending ?
       <h1>Loading</h1> :
       (
         <div className='tc'>
